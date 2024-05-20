e42bba803ab785ca60b15b01fc9b808be4207f70
281278 <157749804+281278@users.noreply.github.com>
2024-05-20 20:42:41 +0200

    Add files via upload

---
+++ Homework_140524/script/script.js
@@ -0,0 +1,24 @@
+// let varibleName = 'Bedeutung';
+
+let number = Number(prompt("yournumber","0"));
+ console.log(number**3);
+
+let number1 =Number(prompt("number1","0"));
+let number2 = Number(prompt("number2","0"));
+
+console.log("sum:"+number1 + number2);
+
+
+
+let user_name = prompt('userName',"userName");
+let user_surname = prompt('userSurname',"usersurname");
+let user_age = Number(prompt('age',"age"));
+let user_position = prompt('position',"position")
+
+const today=new Date();
+const _Year=today.getFullYear();
+
+
+
+console.log("Hi, my name is " + user_name+ " " + user_surname + ". I was born in " + _Year + ".Age:" + user_age + ".I'm "+ user_position);
+
