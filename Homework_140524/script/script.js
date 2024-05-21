c36c01adaa6d4df13e0ae089b5a4875b41bc766a
281278 <157749804+281278@users.noreply.github.com>
2024-05-21 23:19:13 +0200

    Add files via upload

--- Homework_140524/script/script.js
+++ Homework_140524/script/script.js
@@ -12,13 +12,17 @@ console.log("sum:"+number1 + number2);
 
 let user_name = prompt('userName',"userName");
 let user_surname = prompt('userSurname',"usersurname");
-let user_age = Number(prompt('age',"age"));
+let user_age = Number(prompt('age',"0"));
 let user_position = prompt('position',"position")
 
+
+
 const today=new Date();
-const _Year=today.getFullYear();
+const _Year=today.getFullYear();5
+
+let _bornYear = _Year-user_age;
 
 
 
-console.log("Hi, my name is " + user_name+ " " + user_surname + ". I was born in " + _Year + ".Age:" + user_age + ".I'm "+ user_position);
+console.log("Hi, my name is " + user_name+ " " + user_surname + ". I was born in " + _bornYear + ".Age:" + user_age + ".I'm "+ user_position);
 
